bcdab2a58c140d3dac4ffdbb3669f6964b46907a
Dmitrii <dshitikov@rtech.ru>
2019-11-16 21:51:24 +0300

    refactoring Task

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -4,38 +4,22 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
-use Src\classes\Task;
+use TaskForce\classes\Task;
 
 
 class TaskTest extends TestCase
 {
-    /**
-     * Статусы
-     */
-    //CONST STATUS_NEW = 'Новое';
-    CONST STATUS_CANCEL = 'Отменено';
-    CONST STATUS_IN_WORK = 'На исполнении';
-    CONST STATUS_DONE = 'Завершено';
-    CONST STATUS_FAILED = 'Провалено';
-
-    /**
-     * Действия
-     */
-    //CONST ACTION_NEW = 'Добавление задания';
-    CONST ACTION_CANCEL = 'Отменить';
-    CONST ACTION_RESPOND = 'Откликнуться';
-    CONST ACTION_TAKE_IN_WORK = 'Принять';
-    CONST ACTION_DONE = 'Завершение задания';
-    CONST ACTION_REFUSE = 'Отказ от задания';
-
     public function testGetStatus()
     {
         $task = new Task(1);
 
-        $this->assertEquals($task->getNextStatus(self::ACTION_CANCEL), self::STATUS_CANCEL);
-        $this->assertEquals($task->getNextStatus(self::ACTION_RESPOND), false);
-        $this->assertEquals($task->getNextStatus(self::ACTION_TAKE_IN_WORK), self::STATUS_IN_WORK);
-        $this->assertEquals($task->getNextStatus(self::ACTION_DONE), self::STATUS_DONE);
-        $this->assertEquals($task->getNextStatus(self::ACTION_REFUSE), self::STATUS_FAILED);
+        // проверка работы __get()
+        $this->assertEquals($task->clientId, 1);
+
+        $this->assertEquals($task->getNextStatus(Task::ACTION_CANCEL), Task::STATUS_CANCEL);
+        $this->assertEquals($task->getNextStatus(Task::ACTION_RESPOND), false);
+        $this->assertEquals($task->getNextStatus(Task::ACTION_TAKE_IN_WORK), Task::STATUS_IN_WORK);
+        $this->assertEquals($task->getNextStatus(Task::ACTION_DONE), Task::STATUS_DONE);
+        $this->assertEquals($task->getNextStatus(Task::ACTION_REFUSE), Task::STATUS_FAILED);
     }
 }
